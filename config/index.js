0cb39495f425d6da42416501e3eb4f3b983a9cdd
deepam-kapur <deepam@queryit.io>
2023-11-11 14:42:35 +0530

    [ADD] Postgresql DB added

--- config/index.js
+++ config/index.js
@@ -5,7 +5,7 @@ import path from 'path';
 
 const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 
-config({ path: path.normalize(`${__dirname}/../../dynamics/.env`) });
+config({ path: path.normalize(`${__dirname}/../dynamics/.env`) });
 
 const defaultConfig = {
   PORT: 8080,
@@ -14,10 +14,12 @@ const defaultConfig = {
   DISCORD_APPLICATION_ID: '',
   DISCORD_SERVER_ID: '',
   DB_HOST: '127.0.0.1',
-  DB_PORT: 3306,
+  DB_PORT: 5432,
   DB_USERNAME: 'root',
   DB_PASSWORD: 'root',
-  DB_POOL_CONNECTION_LIMIT: 5
+  DB_POOL_CONNECTION_LIMIT: 5,
+  DB_IDLE_TIMEOUT_MILLIS: 10000,
+  DB_CONNECTION_TIMEOUT_MILLIS: 2000
 };
 
 const args = process.argv.slice(2);
--- modules/initialization/init.js
+++ modules/initialization/init.js
@@ -1,30 +1,31 @@
-import QueryXDB from '../common/queryx.db.js'
+import QueryXConnection, {checkDB,} from '../common/queryx.db.js'
 
 const _initiateConnectionsTable = async (connection) => {
     await connection.query(`
        CREATE TABLE IF NOT EXISTS connections (
-        id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-        display_name VARCHAR(255) NOT NULL,
-        type VARCHAR(255) NOT NULL,
-        host VARCHAR(255) NOT NULL DEFAULT '127.0.0.1',
-        port VARCHAR(6) NOT NULL DEFAULT 0,
-        username VARCHAR(255) NOT NULL,
-        password VARCHAR(512) NOT NULL,
-        created_at TIMESTAMP NOT NULL,
-        updated_at TIMESTAMP NOT NULL,
-        PRIMARY KEY (id),
-        UNIQUE INDEX display_name_UNIQUE (display_name ASC) VISIBLE)
+            id SERIAL PRIMARY KEY,
+            display_name VARCHAR(255) NOT NULL,
+            type VARCHAR(255) NOT NULL,
+            host VARCHAR(255) NOT NULL DEFAULT '127.0.0.1',
+            port VARCHAR(6) NOT NULL DEFAULT '0',
+            username VARCHAR(255) NOT NULL,
+            password VARCHAR(512) NOT NULL,
+            created_at TIMESTAMP NOT NULL,
+            updated_at TIMESTAMP NOT NULL
+        )
     `);
+    await connection.query(`CREATE UNIQUE INDEX IF NOT EXISTS display_name_UNIQUE ON connections (display_name)`);
 }
 
 const _initDB = async () => {
-    const connection = QueryXDB();
+    const connection = QueryXConnection;
     await _initiateConnectionsTable(connection);
     
     console.log('Initialized QueryX DB');
 };
 
 const initialize = async () => {
+    await checkDB()
     await _initDB();
 };
 
